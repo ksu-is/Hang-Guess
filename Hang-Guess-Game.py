58f3ea3868511dc90b57611d63d2a405afca3ed5
HunterG10 <59638165+HunterG10@users.noreply.github.com>
2020-04-16 16:58:44 -0400

    2
    A selection process for length of the word you want and how many guesses you want

--- Hang-Guess-Game.py
+++ Hang-Guess-Game.py
@@ -1,11 +1,11 @@
 import time
 import random
 
-easy = ['','','','','']
+Short = ['','','','','']
 
-normal = ['','','','','','']
+Medium = ['','','','','','']
 
-expert = ['','','','','','']
+Long = ['','','','','','']
 
 def mask_word(word):
     anon_word = '_' * len(word)
@@ -19,3 +19,23 @@ def known_word(word, anon_word, guess):
             anon[i] = guess
         i += 1
     return "".join(anon)
+
+def choosenword(word_list):
+    word = random.randint(0, len(word_list) - 1)
+    return word_list[word]
+
+def List(num):
+    if num == "1":
+        return Short
+    if num == "2":
+        return Medium
+    if num == "3":
+        return Long
+
+def difficulty(num):
+    if num == "1":
+        return 10
+    if num == "2":
+        return 7
+    if num == "3":
+        return 4
