272dccf2be617e1d0ed546a064a215329b6c6c07
HunterG10 <59638165+HunterG10@users.noreply.github.com>
2020-04-16 16:51:28 -0400

    Start
    Starting to define what a letter not known looks like and how to join the correct guess to the anonymous ones

--- Hang-Guess-Game.py
+++ Hang-Guess-Game.py
@@ -1,1 +1,21 @@
+import time
+import random
 
+easy = ['','','','','']
+
+normal = ['','','','','','']
+
+expert = ['','','','','','']
+
+def mask_word(word):
+    anon_word = '_' * len(word)
+    return anon_word
+
+def known_word(word, anon_word, guess):
+    i = 0
+    anon = list(anon_word)
+    for w in word:
+        if guess.lower() == w:
+            anon[i] = guess
+        i += 1
+    return "".join(anon)
